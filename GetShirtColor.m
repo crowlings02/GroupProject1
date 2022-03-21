3ae44934452b7a437f0d40a99b6ce7017b2487b8
crowlings02 <77941819+crowlings02@users.noreply.github.com>
2022-03-21 18:53:38 -0400

    Update: Alexis HSV thresholds code

--- GetShirtColor.m
+++ GetShirtColor.m
@@ -20,13 +20,6 @@ title('Detected upper bodies');
 % Store matrix of colors of each pixel from the bottom row of the upper body bounding box
 colorArray = getColorArray(I2,bboxBody);
 
-% for c = 1:size(colorArray,2)
-% colorNameArray = fuzzycolor(colorArray(:,c)');
-% end
-
-% isc = fuzzycolor(colorArray','neutral')>0.5;
-% figure
-% displaycolorpatches(colorArray(isc,:))
 
 
 figure
@@ -37,4 +30,48 @@ displaycolorpatches(colorArray')
 %   Columns 9 through 13
 %     9'magenta' 10'black' 11'white' 12'purple' 13'brown'
 
+% 25 to 45 orange
+% 45 to 65 yellow
+% 65 to 170 green
+% 170 to 190 cyan
+% 160 to 260 blue
+% 260 to 290 purple
+% 290 to 340 pink
+% 340 to 360 red
+% 0 to 25 red
+% Black: value is 10% or below
+% Gray: below 15% Sat, value between 10 and 90%
+% White: below 15% Sat and above 90% value
+
+hsv_array = getHSVarray(colorArray);
+
+ColorNameArray = [];
+for rx = 1:size(hsv_array, 1)
+if hsv_array(rx, 3) <= .1
+    color_name = 'Black';
+elseif hsv_array(rx, 2) <= .15 && hsv_array(rx, 3) >= .1 && hsv_array(rx, 3) <= .9
+    color_name = 'Gray';
+elseif hsv_array(rx, 2) <= .15 && hsv_array(rx, 3) > .9
+    color_name = 'White';
+elseif hsv_array(rx, 1) >= 0 && hsv_array(rx, 1) < 25
+    color_name = 'Red';
+elseif hsv_array(rx, 1) >= 25 && hsv_array(rx, 1) < 45
+    color_name = 'Orange';
+elseif hsv_array(rx, 1) >= 45 && hsv_array(rx, 1) < 65
+    color_name = 'Yellow';
+elseif hsv_array(rx, 1) >= 65 && hsv_array(rx, 1) < 170
+    color_name = 'Green';
+elseif hsv_array(rx, 1) >= 170 && hsv_array(rx, 1) < 190
+    color_name = 'Cyan';
+elseif hsv_array(rx, 1) >= 190 && hsv_array(rx, 1) < 260
+    color_name = 'Blue';
+elseif hsv_array(rx, 1) >= 260 && hsv_array(rx, 1) < 290
+    color_name = 'Purple';
+elseif hsv_array(rx, 1) >= 290 && hsv_array(rx, 1) <= 340
+    color_name = 'Pink';
+elseif hsv_array(rx, 1) > 340 && hsv_array(rx, 1) <= 360
+    color_Name = 'Red';
+end  
+    ColorNameArray = [ColorNameArray, color_name]
+end
 
